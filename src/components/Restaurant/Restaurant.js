3e25dd4dd2a9b8a2c399268dea6ae2939abadcc8
Mike Fallesen <mfallesen2010@gmail.com>
2020-12-23 11:51:53 -0800

    GoogleMaps component Integration

--- src/components/Restaurant/Restaurant.js
+++ src/components/Restaurant/Restaurant.js
@@ -1,15 +1,36 @@
 import React from 'react'
+import { Card, CardContent, Icon, Image } from 'semantic-ui-react'
+import GoogleMaps from '../GoogleMap/GoogleMap'
+
 
 export default function Restaurant() {
     return (
-        <div>
-            <h1 id="restaurant_name">Hello World</h1>
-            <img id='restaurant_thumb' src='https://place-puppy.com/300x300' alt="placeholder"/>
-            <p id='address'>Restaurant Address</p>
-            <img id='restaurant_loc' src='https://place-puppy.com/300x150' alt="placeholder"/>
-            <p id='hours'>Restaurant Hours</p>
-            <p id='menu'>Restaurant Menu</p>
+        <Card centered>
+            <Image src='https://place-puppy.com/300x300'></Image>
+            <Card.Content>
+                <h1 id="restaurant_name">Hello World</h1>
 
-        </div>
+            </Card.Content>
+            <Card.Description>
+
+                <p id='hours'>Restaurant Hours</p>
+                <p id='menu'>Restaurant Menu</p>
+            </Card.Description>
+            <Card.Content >
+
+            <p id='address'>Restaurant Address</p>
+            {/* <Image fluid src='https://place-puppy.com/300x150'></Image> */}
+        <GoogleMaps></GoogleMaps>
+            
+            </Card.Content>
+
+
+
+
+        </Card>
+
+
+
+
     )
 }
