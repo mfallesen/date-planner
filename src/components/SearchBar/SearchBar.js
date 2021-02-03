3996952552bd5c8dc9631b69fd7cdad1a23ef487
Mike Fallesen <mfallesen2010@gmail.com>
2021-02-02 18:31:41 -0800

    enable state in searchBar Component

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,23 +1,32 @@
-import React from 'react'
-import { Button, Checkbox, Container, Form } from 'semantic-ui-react'
+import React, { useState } from 'react'
+import { Button, Checkbox, Container, Form, } from 'semantic-ui-react'
+import './SearchBar.css'
 
 
 
 
 export default function SearchBar(props) {
 
-    return (
-        <Container>
+    const preventDefault = (event) => event.preventDefault();
 
-            <Form size='large'>
-                <Form.Field>
-                    <label> Where can we take you tonight?</label>
-                    <input placeholder='Search City Here!' />
-                    <Checkbox label='Lets do a show afterwards'/>
-                </Form.Field>
-                
-                <Button primary type='submit' >Find Dinner!</Button>
-            </Form>
+    
+
+    return (
+        <Container  className='shrink-down bg-blur'>
+            <Container className='shrink-down'>
+
+                <Form inverted size='large'>
+                    <Form.Field>
+                        <label id='search-header'> Where can we take you tonight?</label>
+                        <input onChange={props.onCityInputChange} value={props.city} name='city' placeholder='Search City Here!' />
+                    </Form.Field>
+                    <Form.Field>
+                        <Checkbox label='Lets do a show afterwards' />
+                    </Form.Field>
+
+                    <Button primary type='submit' >Find Dinner!</Button>
+                </Form>
+            </Container>
         </Container>
     )
 }
