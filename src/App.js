c35f3a5d59e999d59d87bab42820645bf5c9a780
Mike Fallesen <mfallesen2010@gmail.com>
2021-02-18 17:01:10 -0800

    utilize restaurant and weather returns from API call and modify how components are rendered

--- src/App.js
+++ src/App.js
@@ -6,35 +6,57 @@ import Restaurant from './components/Restaurant';
 import Movie from './components/Movie'
 import API from './utils/API'
 import Weather from './components/Weather';
+import { Dimmer, Loader } from 'semantic-ui-react';
+
 
 
 
 function App() {
-
-
   const [city, setCity] = useState('')
+  const [restaurant, setRestaurant] = useState({})
+  const [weather, setWeather] = useState({})
 
+  const [dataLoad, setDataLoad] = useState(false)
+  const [show, setShow] = useState(false)
 
   const registerInputChange = event => {
     const { value } = event.target;
     setCity(value)
   }
 
+  const handleDateSearch = event => {
+    event.preventDefault();
+    setDataLoad(true);
+    
+
+    API.findDateLocation(city).then(response => {
+      setRestaurant(response.data[0].restaurant);
+      setWeather(response.data[1])
+      // restaurant(setRestaurant)
+      console.log(restaurant);
+      console.log(weather);
+    }).catch(err => {
+      console.error(err);
+    }).finally(() => {
+      setDataLoad(false);
+      setShow(true)
+    })
+  }
+
+
+console.log('=======', restaurant)
   return (
     <div className="App">
+      
 
-
-
-      {/* <Weather/> */}
-
-      <SearchBar onCityInputChange={registerInputChange} city={city}></SearchBar>
-      {/* <Restaurant></Restaurant> */}
-
-      {/* Render based on input */}
-      {/* <Movie/> */}
-
+      <SearchBar handleDateSearch={handleDateSearch} onCityInputChange={registerInputChange} city={city}></SearchBar>
+      {dataLoad && <Loader size={'big'} active inverted content="Finding Date" />}
+      {!dataLoad && show  && <Restaurant restaurant={restaurant}></Restaurant>}
+      {!dataLoad && Object.entries(weather) && <Weather weather={weather} city={city}/>}
+      {/* <Movie/>  Future Implementation */}
 
       <Footer></Footer>
+      
     </div>
   );
 }
