d426818e3f537758d545b43b1f66415d693593bb
Mike Fallesen <mfallesen2010@gmail.com>
2021-02-18 16:58:10 -0800

    remove console logs needed for testing

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -15,14 +15,14 @@ export default function SearchBar(props) {
         <Container  className='shrink-down bg-blur'>
             <Container className='shrink-down'>
 
-                <Form inverted size='large'>
+                <Form inverted size='large' onSubmit={props.handleDateSearch}>
                     <Form.Field>
                         <label id='search-header'> Where can we take you tonight?</label>
                         <input onChange={props.onCityInputChange} value={props.city} name='city' placeholder='Search City Here!' />
                     </Form.Field>
-                    <Form.Field>
+                    {/* <Form.Field>
                         <Checkbox label='Lets do a show afterwards' />
-                    </Form.Field>
+                    </Form.Field> */}
 
                     <Button primary type='submit' >Find Dinner!</Button>
                 </Form>
--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import { Card, Image } from 'semantic-ui-react'
 
-export default function Weather() {
+
+
+export default function Weather(props) {
+    if (!props.weather.weather) {
+        return null
+    } 
+    let weaIco = props.weather.weather[0].icon || '' 
+    let weatherObj = props.weather
+
+    const weatherIcon = `http://openweathermap.org/img/wn/${weaIco}@2x.png`
     return (
         <Card centered>
             <Card.Content>
 
-                <Image floated='right' size='mini' src='https://place-puppy.com/100x100'></Image>
-                <Card.Header textAlign='left'>Weather</Card.Header>
-                <Card.Description textAlign='left'> weather outlook for date</Card.Description>
+                <Image floated='right' size='mini' src={weatherIcon}></Image>
+                <Card.Header textAlign='left'>Current Weather in {props.city}</Card.Header>
+                <Card.Description textAlign='center'>{weatherObj.weather[0].main}</Card.Description>
+                <Card.Content textAlign='center'>{weatherObj.main.temp + ' °F'} </Card.Content>
             </Card.Content>
         </Card>
     )
